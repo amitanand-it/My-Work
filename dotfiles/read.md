8a9d51cd1e6a2585a70fb1cd3869d168559b5bb7
amit anand <amit.a@tradeindia.com>
2022-01-21 00:16:41 +0530

    dotfiles have been added

---
+++ dotfiles/read.md
@@ -0,0 +1,3 @@
+
+set sync_dotfiles.sh in cron so that auto update can run
+and if any changes found git can catch this.
---
+++ dotfiles/sync_dotfiles.sh
@@ -0,0 +1,8 @@
+
+
+cp ~/.bash_profile 	~/mywork/dotfiles/bash_profile
+cp ~/.vimrc		~/mywork/dotfiles/vimrc
+cp ~/.inputrc 		~/mywork/dotfiles/inputrc
+
+
+
