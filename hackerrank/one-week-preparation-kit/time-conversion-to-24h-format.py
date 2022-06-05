641187289ae410fb57e74be7e82b055ad0206f61
amit anand <amit.a@tradeindia.com>
2022-06-05 20:31:33 +0530

    1 week interview kit question

---
+++ hackerrank/one-week-preparation-kit/time-conversion-to-24h-format.py
@@ -0,0 +1,62 @@
+def timeConversion(s):
+    # Write your code here
+    tt, am_pm = s[:-2], s[-2:]
+    hh, mm, ss = tt.split(":")
+    if am_pm == 'PM':
+        hh = int(hh)
+        if hh != 12 and hh >= 1:
+            hh += 12
+        return f"{hh}:{mm}:{ss}"
+    
+    if am_pm == 'AM':
+        if hh == "12":
+            hh = '00'
+
+        return f"{hh}:{mm}:{ss}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+"""
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+Constraints
+
+All input times are valid
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45
+
+
+"""
